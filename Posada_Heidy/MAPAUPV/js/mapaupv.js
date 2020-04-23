f39489ec6547b824ac5f16072453f00b65023480
heipofan <hbpfudistrital@gmail.com>
2020-04-23 18:38:40 -0500

    Update mapaupv.js

--- Posada_Heidy/MAPAUPV/js/mapaupv.js
+++ Posada_Heidy/MAPAUPV/js/mapaupv.js
@@ -7,12 +7,12 @@ var marker = null;
 var locText = document.getElementById("locText");
 
 var markerOptions = {
-	radius: 15,
+	radius: 4,
 	color: "black",
 	fillColor: '#fff',
 	fillOpacity: 0.8
 };
-var dataJSON
+var dataJSON;
 var pointLtd = null; // Latitud del punto de ubicación
 var pointLng = null; // Longitud del punto de ubicación
 var myArraymin = [];
@@ -23,23 +23,47 @@ var options = {
 };
 
 // MAPA
-function onDeviceReady() {
+
+async function onDeviceReady() {
+	
+	let promise = new Promise(function(resolve, reject) { 
 		
 	map.setView([39.481000,  -0.341000], 16);
 		var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
 	}).addTo(map);
 
-    var xhr = new XMLHttpRequest() //objeto petición;
-	xhr.open("GET", url+end+"?idPattern=uprv&type=Campus&orderBy=id", true);
+    var xhr = new XMLHttpRequest()
+	xhr.open("GET", url+end+"?limit=1000&type=Edificio", true);
 	xhr.send();
 	
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState ==4 && xhr.status==200){
-			var jsonData = JSON.parse(xhr.responseText);
-			console.log(jsonData);
-			
+			var jsonData = xhr.responseText;
+			dataJSON = JSON.parse(jsonData);
+			for (var i=0; i<dataJSON.length; i++) {
+				//console.log(dataJSON[i].location.value.coordinates);
+				var latpto = dataJSON[i].location.value.coordinates[1]
+				var lonpto = dataJSON[i].location.value.coordinates[0]
+				
+				var markID = L.circle([latpto, lonpto], markerOptions)
+				markID.addTo(map);
+			resolve(dataJSON);
+			}
+		} else {
+					// Normalmente error porque el servidor no existe o no se encuentra...
+			resolve([]);
+			}
+		}
+	});
+};
+	
+	/*for (var i=0; i<pos.length; i++) 
+
 			/*
+			n = dataJSON.pos.;
+			console.log(n);
+			
 			for (var i=0; i<n; i++){
 				var latpto = dataJSON.features[i].geometry.coordinates[1]
 				var lonpto = dataJSON.features[i].geometry.coordinates[0]
@@ -49,7 +73,7 @@ function onDeviceReady() {
 				markID.addTo(map);
 				markID.bindPopup('<p>Price: '+ precio +'</p>').openPopup();
 			}
-			*/	
+			
 					
 		}
 	};
@@ -73,11 +97,11 @@ function onDeviceReady() {
 
 	L.control.layers(baseLayers, overlayMaps, {position: 'bottomright'}).addTo(map);
 }
-
+*/
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  //document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
